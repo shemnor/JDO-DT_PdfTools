e81507c63e286f64f47919f181a459e21327aa5e
Noremberg <shem.noremberg@atkinsglobal.com>
2020-12-01 21:37:50 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace JDO_DT_PdfTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_selectFiles = new System.Windows.Forms.Button();
+            this.btn_modify = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ckbx_createNewNames = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_author = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ckbc_revisionBlock = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbl_progress = new System.Windows.Forms.Label();
+            this.modifyPdfs = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btn_selectFiles
+            // 
+            this.btn_selectFiles.Location = new System.Drawing.Point(12, 12);
+            this.btn_selectFiles.Name = "btn_selectFiles";
+            this.btn_selectFiles.Size = new System.Drawing.Size(75, 23);
+            this.btn_selectFiles.TabIndex = 0;
+            this.btn_selectFiles.Text = "Select Files";
+            this.btn_selectFiles.UseVisualStyleBackColor = true;
+            this.btn_selectFiles.Click += new System.EventHandler(this.btn_selectFiles_Click);
+            // 
+            // btn_modify
+            // 
+            this.btn_modify.Location = new System.Drawing.Point(93, 12);
+            this.btn_modify.Name = "btn_modify";
+            this.btn_modify.Size = new System.Drawing.Size(75, 23);
+            this.btn_modify.TabIndex = 1;
+            this.btn_modify.Text = "Modify";
+            this.btn_modify.UseVisualStyleBackColor = true;
+            this.btn_modify.Click += new System.EventHandler(this.btn_modify_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 65);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(301, 297);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // ckbx_createNewNames
+            // 
+            this.ckbx_createNewNames.AutoSize = true;
+            this.ckbx_createNewNames.Location = new System.Drawing.Point(207, 16);
+            this.ckbx_createNewNames.Name = "ckbx_createNewNames";
+            this.ckbx_createNewNames.Size = new System.Drawing.Size(106, 17);
+            this.ckbx_createNewNames.TabIndex = 3;
+            this.ckbx_createNewNames.Text = "Rename all files?";
+            this.ckbx_createNewNames.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 416);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "For help contact Shem.Noremberg@atkinsglobal.com (2020)";
+            // 
+            // tb_author
+            // 
+            this.tb_author.Location = new System.Drawing.Point(57, 39);
+            this.tb_author.Name = "tb_author";
+            this.tb_author.Size = new System.Drawing.Size(111, 20);
+            this.tb_author.TabIndex = 5;
+            this.tb_author.Text = "XX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Author:";
+            // 
+            // ckbc_revisionBlock
+            // 
+            this.ckbc_revisionBlock.AutoSize = true;
+            this.ckbc_revisionBlock.Location = new System.Drawing.Point(207, 38);
+            this.ckbc_revisionBlock.Name = "ckbc_revisionBlock";
+            this.ckbc_revisionBlock.Size = new System.Drawing.Size(108, 17);
+            this.ckbc_revisionBlock.TabIndex = 7;
+            this.ckbc_revisionBlock.Text = "Insert rev. block?";
+            this.ckbc_revisionBlock.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 381);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(303, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // lbl_progress
+            // 
+            this.lbl_progress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbl_progress.AutoSize = true;
+            this.lbl_progress.Location = new System.Drawing.Point(12, 365);
+            this.lbl_progress.Name = "lbl_progress";
+            this.lbl_progress.Size = new System.Drawing.Size(65, 13);
+            this.lbl_progress.TabIndex = 9;
+            this.lbl_progress.Text = "Progress 0%";
+            // 
+            // modifyPdfs
+            // 
+            this.modifyPdfs.WorkerReportsProgress = true;
+            this.modifyPdfs.DoWork += new System.ComponentModel.DoWorkEventHandler(this.modifyPDFs_DoWork);
+            this.modifyPdfs.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.modifyPDFs_ProgressChanged);
+            this.modifyPdfs.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.modifyPDFs_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 436);
+            this.Controls.Add(this.lbl_progress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.ckbc_revisionBlock);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_author);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ckbx_createNewNames);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_modify);
+            this.Controls.Add(this.btn_selectFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(300, 400);
+            this.Name = "Form1";
+            this.Text = "JDO-DT Pdf Tools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_selectFiles;
+        private System.Windows.Forms.Button btn_modify;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox ckbx_createNewNames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_author;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox ckbc_revisionBlock;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbl_progress;
+        private System.ComponentModel.BackgroundWorker modifyPdfs;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using static PdfParser.PdfParser;
+
+namespace JDO_DT_PdfTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //GLOBAL VARS
+        string[] files;
+
+        //STANDARD ERROR MESSAGES
+        private static class ErrorMessages
+        {
+            public static String changingColors { get { return "An error occured when changing colors. It wont be possible to use the {0} drawing and it will be skipped. \n Would you like to resume the process for the remaining drawings?"; } }
+            public static String addingRevisionBlock { get { return "An error occured when adding the revision title block. \n Would you like to resume the process for the remaining drawings?"; } }
+            public static String fileNotFound { get { return "Drawing you want to edit was not found. \n Would you like to resume the process for the remaining drawings?"; } }
+        }
+
+        //BUTTONS
+        private void btn_selectFiles_Click(object sender, EventArgs e)
+        {
+            files = getPdfFilesFromUser();
+
+            if (files != null)
+            {
+                //empty list of files
+                richTextBox1.Text = "";
+
+                //reset progressbar
+                _setupProgressbar();
+
+                //append new file list
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    richTextBox1.AppendText(fileName + "\r\n");
+                }
+            }
+        }
+        private void btn_modify_Click(object sender, EventArgs e)
+        {
+            //if already running exit
+            if (modifyPdfs.IsBusy) { return; }
+            
+            //if not files selected exit
+            if(files == null) { return; }
+
+            //setup progressbar
+            _setupProgressbar();
+
+            //modify pdfs in background
+            modifyPdfs.RunWorkerAsync();
+        }
+
+        //WORK
+        private void modifyPDFs_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                //increment progress
+                worker.ReportProgress((i+1) * (100 / files.Length));
+
+                //get source drawing path
+                if (!File.Exists(files[i]))
+                {
+                    DialogResult result = MessageBox.Show(String.Format(ErrorMessages.fileNotFound, ""), "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    worker.ReportProgress(0);
+                    if (result.Equals(DialogResult.No)) {e.Cancel = true; break;}
+                    continue;
+                }
+                string sourcePath = files[i];
+
+                //change Annot colors
+                string destPath = generateTempFileName(sourcePath, ckbx_createNewNames.Checked, i);
+                bool colorChangeComplete = changeColorOfAnnotsByColor(sourcePath, destPath, Color.Red, Color.Black);
+
+                //handle errors
+                if (!colorChangeComplete)
+                {
+                    if (File.Exists(destPath)) { File.Delete(destPath); }
+                    DialogResult result = MessageBox.Show(String.Format(ErrorMessages.changingColors, ""), "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result.Equals(DialogResult.No)) { e.Cancel = true; break; }
+                    continue;
+                }
+                cleanupFiles(sourcePath, destPath, ckbx_createNewNames.Checked);
+
+                //add revision title block
+                if (ckbc_revisionBlock.Checked)
+                {
+                    //check which filename was used
+                    string tempPath = "";
+                    if (ckbx_createNewNames.Checked)
+                    {tempPath = generateTempFileName(destPath, false, i);}
+                    else
+                    {tempPath = generateTempFileName(sourcePath, false, i);}
+
+                    string revision = destPath.Substring(destPath.LastIndexOf('.') - 4, 4);
+                    bool revTitleAdded = addRevisionTitleblock(destPath, tempPath, revision, tb_author.Text);
+
+                    // handle errors
+                    if (!revTitleAdded)
+                    {
+                        if (File.Exists(tempPath)) { File.Delete(tempPath); }
+                        DialogResult result = MessageBox.Show(String.Format(ErrorMessages.addingRevisionBlock, ""), "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result.Equals(DialogResult.No)) { e.Cancel = true; break; }
+                        continue;
+                    }
+                    cleanupFiles(destPath, tempPath, false);
+                }
+            }
+        }
+        private void modifyPDFs_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            _updateProgressbar(e.ProgressPercentage);
+        }
+        private void modifyPDFs_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //if cancelled update bar to say cancelled
+            if (e.Cancelled) { _updateProgressbar(0); return; }
+
+            //else wait for explorer to update and finalise
+            Thread.Sleep(500);
+            _finaliseProgressbar();
+        }
+
+        //USER INTERFACE
+        private bool validateUserFileSelection(string[] files, string extension)
+        {
+            foreach (string file in files)
+            {
+                if (!file.Contains(extension))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private bool validateUserFileSelection(string file, string extension)
+        {
+            if (!file.Contains(extension))
+            {
+                return false;
+            }
+            return true;
+        }
+        private void _setupProgressbar ()
+        {
+            progressBar1.Maximum = 100;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+            lbl_progress.Text = "Progress 0%";
+        }
+        private void _updateProgressbar(int currentPercentage)
+        {
+            // if returned by 0 means interrupted
+            if(currentPercentage == 0)
+            {
+                lbl_progress.Text = "User Interrupted";
+                progressBar1.Value = 0;
+                return;
+            }
+            //else update normally
+            lbl_progress.Text = String.Format("Progress {0}%", currentPercentage);
+            progressBar1.Value = currentPercentage;
+        }
+        private void _finaliseProgressbar()
+        {
+            progressBar1.Value = progressBar1.Maximum;
+            lbl_progress.Text = "Progress 100% - Complete";
+        }
+
+        //HELPER METHODS
+        private static string[] getPdfFilesFromUser()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog 
+            {
+                Title = "Please select .pdf files to edit",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\OneDrive Corp\\Atkins Ltd\\UK1226_SDLT - Documents",
+                Filter = "Pdf Files|*.pdf",
+                FilterIndex = 1,
+                Multiselect = true,
+                RestoreDirectory = true
+            };
+
+            DialogResult result = fileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                return fileDialog.FileNames;
+            }
+            return null;
+        }
+        private static void cleanupFiles(string originalFile, string tempFile, bool keepNewName)
+        {
+            //replace or delete files
+            if (keepNewName)
+            {
+                File.Delete(originalFile);
+            }
+            else
+            {
+                File.Delete(originalFile);
+                File.Move(tempFile, originalFile);
+            }
+        }
+        private static string generateTempFileName(string sourcePath, bool createNewName, int fileIndex)
+        {
+            //create temp drawing name for editing
+            string sourceDir = Path.GetDirectoryName(sourcePath);
+            string sourceFileName = Path.GetFileNameWithoutExtension(sourcePath);
+            string tempPath = Path.Combine(sourceDir, sourceFileName) + "(1).pdf";
+
+            // if user requests renaming the file generate new fileName
+            if (createNewName)
+            {
+                string sketch = String.Format("SKETCH {0,2:D2}", fileIndex + 1);
+                string name = sourceFileName.Substring(
+                    sourceFileName.IndexOf("HPC"),
+                    sourceFileName.LastIndexOf("-") - sourceFileName.IndexOf("HPC"));
+                string rev = getNextRevisionFromDocument(sourcePath);
+                tempPath = String.Format("{0}\\{1} {2}-{3}.pdf", sourceDir, sketch, name, rev);
+            }
+
+            //generate stream
+            FileInfo fileInfo = new FileInfo(tempPath);
+            fileInfo.Directory.Create();
+            File.Copy(sourcePath, tempPath);
+            
+            return tempPath;
+        }
+    }
+}
